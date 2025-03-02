76ef7a2e97984988474a14bab1a4d6f8a3b729c3
randallkrouth <randy.krouth@gmail.com>
2025-03-01 20:02:24 -0500

    Added cart functionality in CartItem.jsx and ProductList.jsx

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,42 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let total = 0.0;
+    cart.forEach(item => 
+    {
+        total += item.quantity * parseFloat(item.cost.substring(1));
+    })
+    return total.toFixed(2);
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e);
   };
 
-
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+  };
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+    if(item.quantity >= 1)
+    {
+        dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+    }
   };
 
   const handleRemove = (item) => {
+       dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    const unitPrice = parseFloat(item.cost.substring(1));
+    const subTotal = item.quantity * unitPrice;
+    return subTotal.toFixed(2);
   };
 
   return (
